19e9c6fe4aa9654388ca07f29fa04a5b2212f863
YongSheng109 <93895098+YongSheng109@users.noreply.github.com>
2021-11-18 19:29:32 +0800

    Update Code1.sh

--- Core/Code1.sh
+++ Core/Code1.sh
@@ -1,6 +1,3 @@
-#!bin/bash
-#!storage/downloads/YongScript-v1
-
 black="\033[1;30m"
 red="\033[1;31m"
 green="\033[1;32m"
@@ -23,15 +20,14 @@ cd storage
 mkdir Hack
 clear
 
-figlet -f small YongScript
+figlet -f small YongScript v1
 echo -e "${green}Enter Your Choice :"
 echo -e "${blue}[1] CamHacker"
 echo -e "${blue}[2] LiteDdos"
 echo -e "${blue}[3] NitroGen"
-echo -e "${blue}[4] Raid-ToolBox"
 read OwO
-if [ $OwO -eq 1 ];
-then
+if [ $OwO -eq 1 ]
+then aq
 cd
 cd storage
 cd Hack
@@ -40,9 +36,8 @@ cd
 cd storage
 cd YongScript-v1
 clear
-bash Start.sh
-read OwO
-elif [ $OwO -eq 2 ];
+bash Code1.sh
+elif [ $OwO -eq 2 ]
 then
 cd
 cd storage
@@ -52,36 +47,19 @@ cd
 cd storage
 cd YongScript-v1
 clear
-bash Start.sh
-read OwO
-elif [ $OwO -eq 3 ];
+bash Code1.sh
+elif [ $OwO -eq 3 ]
 then
 cd
 cd storage
-cd Hack
+cd Hacks
 git clone https://github.com/YongSheng109/Nitro-gen.git
 cd
 cd storage
 cd YongScript-v1
 clear
-bash Start.sh
-read OwO
-elif [ $OwO -eq 4 ];
-then
-cd
-cd storage
-cd Hack
-git clone https://github.com/DeadBread76/Raid-Toolbox.git
-cd Raid-ToolBox
-pip install -r requirements_termux.txt
-echo "Type 'python RTB.py no_gui' To Start. You Need To CTRL C After Installed Termux-Api"
-sleep 10
-cd
-cd storage
-cd YongScript-v1
-clear
-bash Start.sh
-elif [ $OwO -gt 4 ]
+bash Code1.sh
+elif [ $OwO -gt 3 ]
 then
 echo -e "${green}Invalid Choices"
 fi
