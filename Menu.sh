8759bde19f6cfe29f67b716706e0463b21a704f2
YongSheng109 <93895098+YongSheng109@users.noreply.github.com>
2021-11-18 11:42:32 +0800

    Add files via upload

---
+++ Menu.sh
@@ -0,0 +1,56 @@
+black="\033[1;30m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+cyan="\033[1;36m"
+violate="\033[1;37m"
+white="\033[0;37m"
+nc="\033[00m"
+
+info="${cyan}[${white}+${cyan}] ${yellow}"
+ask="${cyan}[${white}?${cyan}] ${purple}"
+error="${cyan}[${white}!${cyan}] ${red}"
+success="${cyan}[${white}√${cyan}] ${green}"
+
+clear
+
+figlet -f small YongScript v1
+echo -e "${green}Enter Your Choice :"
+echo -e "${cyan}[1] Updates & Packages"
+echo -e "${cyan}[2] Main Menu"
+echo -e "${cyan}[3] Info"
+
+read OwO
+if [ $OwO -eq 1 ]
+then
+cd
+cd storage
+cd YongScript-v1
+cd Core
+bash Code.sh
+clear
+cd
+cd storage
+cd YongScript-v1
+clear
+bash Menu.sh
+elif [ $OwO -eq 2 ]
+then
+cd
+cd storage
+cd YongScript-v1
+bash Start.sh
+elif [ $OwO -eq 3 ]
+then
+cd
+cd storage
+cd YongScript-v1
+clear
+echo -e "${green}Discord: YongSheng_#1976"
+echo -e "${blue}PayPal: https://www.paypal.me/YongShengOwO"
+echo -e "${yellow}Thanks For Using Our Tool"
+sleep 10
+bash Menu.sh
+fi
---
+++ Start.sh
@@ -0,0 +1,4 @@
+#🤣Thers Many Bug So I Made This
+
+sed -i 's/\r//' Code1.sh
+bash Code1.sh
